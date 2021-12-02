17d8430d2ee42fa57581ead22ab1b39b56775c59
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2021-12-02 10:17:21 +0530

    id to _id

--- index.js
+++ index.js
@@ -21,21 +21,15 @@
 import express from "express"; //"type":"module",
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-import { getMovies, createMovies, getMoviesById, deleteMoviesById, editMoviesById } from "./helper.js";
-import {moviesRouter} from "./routes/movies.js";
 import cors from "cors";  
-
-
-
+import { ObjectId } from "mongodb";
 dotenv.config(); // all keys it will put in process.env
 
 const app = express();
-const PORT = process.env.PORT; //heroku will auto assign available port
+const PORT = 9000;
+
 
 app.use(cors());
-
-app.use("/movies",moviesRouter);
-
 //middleware
 app.use(express.json()); // every request in the app body is parsed as json
 //express.json() - inbuilt middleware
@@ -55,14 +49,111 @@ const MONGO_URL = process.env.MONGO_URL;
  }
  //call the function
  //this is for got the client
- export const client = await createConnection();
+ const client = await createConnection();
 
 app.get("/",(request,response)=>{
     response.send("hello happy world");
 });
 
+app.get("/movies", async (request,response)=>{
+    //request->query params
+    console.log(request.query);
+	// const {language,rating} = request.query;
+	const filter = request.query;
+	console.log(filter);
+	if(filter.rating){
+		filter.rating = +filter.rating;
+	}
+	//db.movies.find({language:"tamil",rating:8})
+	//all the movies comment db.movies.find({})
+	// .find({})
+	const filterMovies = await getMovies(filter); 
+	// cursor to array
+	// console.log(filterMovies);
+	//cursor means nothing but pagination 1 2 3 4 5 next->
+	// console.log(language,rating);
 
+	// let filterMovies = movies;
+
+	// if(language){
+	//  filterMovies = filterMovies.filter((mv)=>mv.language === language);
+	// }
+	// if(rating){
+	// 	filterMovies = filterMovies.filter((mv)=>mv.rating === +rating);
+	//    }
+	   response.send(filterMovies);
+});
+
+app.post("/movies", async (request,response)=>{
+const data = request.body;
+// create movies - db.movies.insertMany(data)
+const result = await createMovies(data);
+response.send(result);
+});
+
+// this is creating query and the data we getting from mongodb(database) in postman
+app.get("/movies/:id", async (request,response)=>{
+    console.log(request.params);
+    const {id} = request.params;
+	//db.movies.findOne({id:"102"})
+	const movie = await getMoviesById(id)
+    // const movie = movies.find((mv)=>mv.id === id);
+    console.log(movie);
+
+    movie? response.send(movie) : response.status(404).send({message:"no matching movie found"});
+});
+
+app.delete("/movies/:id", async (request,response)=>{
+    console.log(request.params);
+    const {id} = request.params;
+	const result = await deleteMoviesById(id)
+    console.log(result);
+
+    result.deletedCount>0? response.send(result) : response.status(404).send({message:"no matching movie found"});
+});
+
+app.put("/movies/:id", async (request,response)=>{
+    console.log(request.params);
+    const {id} = request.params;
+	const data = request.body;
+	//db.movies.updateOne({id:id},{$set:data})
+	const result = await editMoviesById(id, data);
+	const movie = await getMoviesById(id);
+    console.log(result);
+	response.send(movie);
+});
 
 app.listen(PORT,()=>console.log("app is started in",PORT));
 
+async function editMoviesById(id, data) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.updateOne({ _id: ObjectId(id) }, { $set: data });
+}
 
+async function deleteMoviesById(id) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.deleteOne({ _id: ObjectId(id) });
+}
+
+async function createMovies(data) {
+	return await client.db("b28wd").collection("movies").insertMany(data);
+}
+
+async function getMoviesById(id) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.findOne({ _id: ObjectId(id) });
+}
+
+async function getMovies(filter) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.find(filter)
+		.toArray();
+}
